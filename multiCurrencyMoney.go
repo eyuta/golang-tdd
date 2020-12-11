102a8946e5d660b7df784ea518f106a2b96a8fbf
eyuta <shion612@gmail.com>
2020-12-11 18:47:39 +0900

    一歩 前進！ テスト に 失敗 し た

--- multiCurrencyMoney.go
+++ multiCurrencyMoney.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+// TODO test2
 
+type Dollar struct {
+	amount int
+}
+
+func (d *Dollar) times(multiplier int) {
+
+}
+func multiCurrencyMoney() {
+
+}
+func main() {
+	// FIXED test
+	//TODO 2
 }
--- multiCurrencyMoney_test.go
+++ multiCurrencyMoney_test.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMultiCurrencyMoney(t *testing.T) {
+	t.Run("$5 * 2 = $10", func(t *testing.T) {
+		five := Dollar{5}
+		five.times(2)
+		assert.Equal(t, 10, five.amount)
+	})
+}
